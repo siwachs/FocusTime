fedea43e7321068453e8edd03f9c8831a1e5820f
siwachs <ssiwach.8888@gmail.com>
2024-01-18 17:30:57 +0530

    refactored

---
+++ src/features/Focus.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export const Focus = () => (
+  <View style={styles.container}>
+    <Text></Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
