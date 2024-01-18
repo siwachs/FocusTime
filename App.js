aac8be0113d7da6a53c9a42ec8f7b3d9188a3bc1
siwachs <ssiwach.8888@gmail.com>
2024-01-18 15:16:26 +0530

    init project

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Text, SafeAreaView, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50,
+  },
+});
